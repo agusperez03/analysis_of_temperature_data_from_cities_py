5dc84290cbf6e8be3da1bd46499c0a727c1cf287
ValentinPardo <dronesdetandil@gmail.com>
2024-05-16 12:36:07 -0300

    medio tpe

--- Inciso2.py
+++ Inciso2.py
@@ -1,4 +1,3 @@
-#implementacion de montecarlo
 
 import numpy as np
 import random
@@ -40,3 +39,126 @@ df1.to_csv('S1_convertido.csv', index=False, header=False)
 df2.to_csv('S2_convertido.csv', index=False, header=False)
 df3.to_csv('S3_convertido.csv', index=False, header=False)
 
+#se calcula la probabilidad de cada rango sobre todas las señales
+def probabilidad(signal):
+    B=0
+    M=0
+    A=0
+    for i in range(0, len(signal)):
+        if signal[i] == 'B':
+            B+=1
+        elif signal[i] == 'M':
+            M+=1
+        else:
+            A+=1
+    probB = B/len(signal)
+    probM = M/len(signal)
+    probA = A/len(signal)
+    salida = [probB, probM, probA]
+    recuento = [B,M,A]
+    return salida, recuento
+
+probBA,recuentoBA = probabilidad(BAconvertido)
+probBO,recuentoBO = probabilidad(BOconvertido)
+probVA,recuentoVA = probabilidad(VAconvertido)
+
+print('La probabilidades de Buenos Aires es:', probBA)
+print('La probabilidades de Bogota es:', probBO)
+print('La probabilidades de Vancouver es:', probVA)
+
+#se calculara la probabilidad con memoria de 1
+def probabilidadMemoria1(signal,divisor):
+    bb=0
+    bm=0
+    ba=0
+    mb=0
+    mm=0
+    ma=0
+    ab=0
+    am=0
+    aa=0
+    for i in range(1, len(signal)):
+        if signal[i] == 'B' and signal[i-1] == 'B':
+            bb +=1
+        elif signal[i] == 'B' and signal[i-1] == 'M':
+            bm +=1
+        elif signal[i] == 'B' and signal[i-1] == 'A':
+            ba +=1
+        elif signal[i] == 'M' and signal[i-1] == 'B':
+            mb +=1
+        elif signal[i] == 'M' and signal[i-1] == 'M':
+            mm +=1
+        elif signal[i] == 'M' and signal[i-1] == 'A':
+            ma +=1
+        elif signal[i] == 'A' and signal[i-1] == 'B':
+            ab +=1
+        elif signal[i] == 'A' and signal[i-1] == 'M':
+            am +=1
+        elif signal[i] == 'A' and signal[i-1] == 'A':
+            aa +=1
+    probBB = bb/divisor[0]
+    probBM = bm/divisor[0]
+    probBA = ba/divisor[0]
+    probMB = mb/divisor[1]
+    probMM = mm/divisor[1]
+    probMA = ma/divisor[1]
+    if divisor[2]!=0 :
+        probAB = ab/divisor[2]
+    else:
+        probAB = 0
+    if divisor[2]!=0 :
+        probAM = am/divisor[2]
+    else:
+        probAM = 0
+    if divisor[2]!=0 :
+        probAA = aa/divisor[2]
+    else:
+        probAA = 0
+    salida = [probBB, probBM, probBA, probMB, probMM, probMA, probAB, probAM, probAA]
+    
+    return salida
+
+probBAMemoria1 = probabilidadMemoria1(BAconvertido,recuentoBA)
+probBOMemoria1 = probabilidadMemoria1(BOconvertido,recuentoBO)
+probVAMemoria1 = probabilidadMemoria1(VAconvertido,recuentoVA)
+
+print('La probabilidades de Buenos Aires con memoria 1 es:', probBAMemoria1)
+print('La probabilidades de Bogota con memoria 1 es:', probBOMemoria1)
+print('La probabilidades de Vancouver con memoria 1 es:', probVAMemoria1)
+
+#calculo de la entropia
+def entropia(prob):
+    s=0
+    for i in range(0, len(prob)):
+        if prob[i] != 0:
+            s = s + prob[i]*np.log2(prob[i])
+    s = -s
+    return s
+
+#sin memoria
+entropiaBA = entropia(probBA)
+entropiaBO = entropia(probBO)
+entropiaVA = entropia(probVA)
+
+print('La entropia de Buenos Aires es:', entropiaBA)
+print('La entropia de Bogota es:', entropiaBO)
+print('La entropia de Vancouver es:', entropiaVA)
+
+#con memoria
+entropiaBAMemoria1 = entropia(probBAMemoria1)
+entropiaBOMemoria1 = entropia(probBOMemoria1)
+entropiaVAMemoria1 = entropia(probVAMemoria1)
+
+print('La entropia de Buenos Aires con memoria 1 es:', entropiaBAMemoria1)
+print('La entropia de Bogota con memoria 1 es:', entropiaBOMemoria1)
+print('La entropia de Vancouver con memoria 1 es:', entropiaVAMemoria1)
+
+
+prueba=''
+prueba = prueba+'A'
+
+print(prueba)
+
+prueba = 'B' +prueba
+
+print(prueba)
